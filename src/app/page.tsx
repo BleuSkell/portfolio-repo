47f990268df53906142b0f5db9c4674a4bd1f6bc
BleuSkell <kyasowi1@gmail.com>
2026-05-15 13:26:38 +0200

    navigation finished, added fonts as well

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import Nav from "@/components/nav";
 
 export default function Home() {
     return (
-        <div className="flex flex-col flex-1 items-center justify-center bg-primary font-sans">
+        <div className="flex flex-col bg-primary font-sans">
             <Nav/>
 
             <main></main>
