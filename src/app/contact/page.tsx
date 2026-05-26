96152468ae744332ff48ca312736565aade001b5
BleuSkell <kyasowi1@gmail.com>
2026-05-26 15:54:48 +0200

    started on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,11 @@
-import Nav from "@/components/nav";
+import Nav from "@/components/nav"
+import Footer from "@/components/footer"
+
+import Link from "next/link";
+import Image from "next/image";
+
+import email from "public/pages/contact/email.png"
+import instagram from "public/pages/contact/instagram.png"
 
 export default function ContactPage() {
     return (
@@ -6,8 +13,24 @@ export default function ContactPage() {
             <Nav/>
 
             <main>
-                <h1>Contact page</h1>
+                <section className="flex flex-row justify-evenly">
+                    <div className="font-instrument-sans">
+                        <h3 className="font-shrikhand text-secondary">Contact</h3>
+                        <p>Have something in mind you're excited about? Let's talk! Wether if it's about code, art, fashion, or simply just have a question or a nice chat, don't be afraid to drop a message!</p>
+                        <p>You can also contact me through Direct Messages on Instagram!</p>
+                        <Link href="https://instagram.com/seervska" target="_blank" className="flex items-center gap-2">
+                            <Image src={instagram} alt="instagram logo" className="w-7 h-7"/>
+                            DM me!
+                        </Link>
+                        <Link href="/" target="_blank" className="flex items-center gap-2">
+                            <Image src={email} alt="email icon" className="w-7 h-7"/>
+                            Send me an E-mail!
+                        </Link>
+                    </div>
+                </section>
             </main>
+
+            <Footer/>
         </div>
     )
 }
